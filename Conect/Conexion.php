fa309fa1bf42b016e83f2defc848626f5ff59b04
Angelo <Angeloponce25@gmail.com>
2025-03-18 23:09:36 -0500

    150325

--- Conect/Conexion.php
+++ Conect/Conexion.php
@@ -4,8 +4,8 @@ namespace Conect;
  date_default_timezone_set('America/Lima'); 
  class Conexion{
         const HOST = 'localhost';
-        const USER = 'simvetec_root';
-        const PASSWORD = 'W?9Cvb=G(o*D';
+        const USER = 'root';
+        const PASSWORD = '';
         const BDNAME = 'simvetec_billing';
     public static function conectar() {
         $link = new \PDO("mysql:host=".self::HOST."; dbname=".self::BDNAME.";",self::USER, self::PASSWORD);
